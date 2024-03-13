9164cd76ddc5b0cef7e56b34406a43eb67952029
VinciVelasco <vincivelasco@gmail.com>
2024-03-13 10:49:39 -0700

    setting up project for channels to work in deployment.

--- services/whiteboard/whiteboard_service/consumers.py
+++ services/whiteboard/whiteboard_service/consumers.py
@@ -8,7 +8,7 @@ class WhiteBoardConsumer(AsyncWebsocketConsumer):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = "chat_%s" % self.room_name
 
-        
+
         # Join room group
         await self.channel_layer.group_add(
             self.room_group_name, self.channel_name
@@ -28,7 +28,7 @@ class WhiteBoardConsumer(AsyncWebsocketConsumer):
         print("got message")
 
         text_data_json = json.loads(text_data)
-        message = text_data_json["data"]
+        message = text_data_json["message"] #"data"
 
         # Send message to room group
         await self.channel_layer.group_send(
