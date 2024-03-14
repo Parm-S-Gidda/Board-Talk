3eb48d3226a4b6900839654fc62e23f2a2e974e4
Ikenna Okpala <ikennajesse77@gmail.com>
2024-03-14 01:16:56 -0700

    Modify endpoints

--- client/board-talk/src/component/board/Board.jsx
+++ client/board-talk/src/component/board/Board.jsx
@@ -1,152 +1,146 @@
-import React from 'react';
-import io from 'socket.io-client';
+import React from "react";
+import io from "socket.io-client";
 
-import './style.css';
+import "./style.css";
 
 class Board extends React.Component {
+  timeout;
+  socket;
 
-    timeout;
-    socket;
+  ctx;
+  isDrawing = false;
 
-   
+  constructor(props) {
+    super(props);
 
-    ctx;
-    isDrawing = false;
+    this.socket = new WebSocket(
+      "wss://" +
+        "whiteboard-service-nf23qxdm2q-uc.a.run.app" +
+        "/ws/whiteboard/" +
+        1 +
+        "/"
+    );
 
-    constructor(props) {
-        super(props);
+    this.socket.onopen = () => {
+      console.log("WebSocket connection established.");
+    };
 
-        this.socket = new WebSocket('wss://'
-        + "whiteboard-service-2pmnanshaq-uw.a.run.app"
-        + '/ws/whiteboard/'
-        + 1
-        + '/');
+    this.socket.addEventListener("message", function (event) {
+      console.log("got message");
 
-        this.socket.onopen = () => {
-            console.log("WebSocket connection established.");
+      console.log(event.data);
+
+      var root = this;
+      var interval = setInterval(function () {
+        if (root.isDrawing) return;
+
+        root.isDrawing = true;
+        clearInterval(interval);
+        var image = new Image();
+        var canvas = document.querySelector("#board");
+        var ctx = canvas.getContext("2d");
+        image.onload = function () {
+          ctx.drawImage(image, 0, 0);
+
+          root.isDrawing = false;
         };
 
-        
-        this.socket.addEventListener('message', function(event){
-            
-            console.log("got message");
+        try {
+          var jsonData = JSON.parse(event.data);
+          var messageValue = jsonData.message;
+        } catch (error) {
+          console.error("Error parsing JSON:", error);
+        }
 
-            console.log(event.data)
+        image.src = messageValue;
+      }, 200);
+    });
+  }
 
-            var root = this;
-            var interval = setInterval(function(){
-         
-                if(root.isDrawing) return;
-          
-                root.isDrawing = true;
-                clearInterval(interval);
-                var image = new Image();
-                var canvas = document.querySelector('#board');
-                var ctx = canvas.getContext('2d');
-                image.onload = function() {
+  componentDidMount() {
+    this.drawOnCanvas();
+  }
 
-                   
-                    ctx.drawImage(image, 0, 0);
+  componentWillReceiveProps(newProps) {
+    this.ctx.strokeStyle = newProps.color;
+    this.ctx.lineWidth = newProps.size;
+  }
 
-                    root.isDrawing = false;
-                };
+  drawOnCanvas() {
+    var canvas = document.querySelector("#board");
+    this.ctx = canvas.getContext("2d");
+    var ctx = this.ctx;
 
-               
+    var sketch = document.querySelector("#sketch");
+    var sketch_style = getComputedStyle(sketch);
+    canvas.width = parseInt(sketch_style.getPropertyValue("width"));
+    canvas.height = parseInt(sketch_style.getPropertyValue("height"));
 
-             
-              
+    var mouse = { x: 0, y: 0 };
+    var last_mouse = { x: 0, y: 0 };
 
-                try {
-                    var jsonData = JSON.parse(event.data);
-                    var messageValue = jsonData.message;
-                 
-                } catch (error) {
-                    console.error('Error parsing JSON:', error);
-                }
-          
-                image.src = messageValue;
+    /* Mouse Capturing Work */
+    canvas.addEventListener(
+      "mousemove",
+      function (e) {
+        last_mouse.x = mouse.x;
+        last_mouse.y = mouse.y;
 
-            }, 200)
-        })
-        
+        mouse.x = e.pageX - this.offsetLeft;
+        mouse.y = e.pageY - this.offsetTop;
+      },
+      false
+    );
 
-        
-    }
+    /* Drawing on Paint App */
+    ctx.lineWidth = this.props.size;
+    ctx.lineJoin = "round";
+    ctx.lineCap = "round";
+    ctx.strokeStyle = this.props.color;
 
-    componentDidMount() {
-        this.drawOnCanvas();
-    }
+    canvas.addEventListener(
+      "mousedown",
+      function (e) {
+        canvas.addEventListener("mousemove", onPaint, false);
+      },
+      false
+    );
 
-    componentWillReceiveProps(newProps) {
-        this.ctx.strokeStyle = newProps.color;
-        this.ctx.lineWidth = newProps.size;
-    }
+    canvas.addEventListener(
+      "mouseup",
+      function () {
+        canvas.removeEventListener("mousemove", onPaint, false);
+      },
+      false
+    );
 
-    drawOnCanvas() {
-        var canvas = document.querySelector('#board');
-        this.ctx = canvas.getContext('2d');
-        var ctx = this.ctx;
+    var root = this;
+    var onPaint = function () {
+      ctx.beginPath();
+      ctx.moveTo(last_mouse.x, last_mouse.y);
+      ctx.lineTo(mouse.x, mouse.y);
+      ctx.closePath();
+      ctx.stroke();
 
-        var sketch = document.querySelector('#sketch');
-        var sketch_style = getComputedStyle(sketch);
-        canvas.width = parseInt(sketch_style.getPropertyValue('width'));
-        canvas.height = parseInt(sketch_style.getPropertyValue('height'));
+      if (root.timeout != undefined) clearTimeout(root.timeout);
+      root.timeout = setTimeout(function () {
+        var base64ImageData = canvas.toDataURL("image/png");
 
-        var mouse = {x: 0, y: 0};
-        var last_mouse = {x: 0, y: 0};
+        root.socket.send(
+          JSON.stringify({ event: "canvas-data", data: base64ImageData })
+        );
+        console.log("sent data");
+      }, 1000);
+    };
+  }
 
-        /* Mouse Capturing Work */
-        canvas.addEventListener('mousemove', function(e) {
-            last_mouse.x = mouse.x;
-            last_mouse.y = mouse.y;
-
-            mouse.x = e.pageX - this.offsetLeft;
-            mouse.y = e.pageY - this.offsetTop;
-        }, false);
-
-
-        /* Drawing on Paint App */
-        ctx.lineWidth = this.props.size;
-        ctx.lineJoin = 'round';
-        ctx.lineCap = 'round';
-        ctx.strokeStyle = this.props.color;
-
-        canvas.addEventListener('mousedown', function(e) {
-            canvas.addEventListener('mousemove', onPaint, false);
-        }, false);
-
-        canvas.addEventListener('mouseup', function() {
-            canvas.removeEventListener('mousemove', onPaint, false);
-        }, false);
-
-        var root = this;
-        var onPaint = function() {
-            ctx.beginPath();
-            ctx.moveTo(last_mouse.x, last_mouse.y);
-            ctx.lineTo(mouse.x, mouse.y);
-            ctx.closePath();
-            ctx.stroke();
-
-            
-            if(root.timeout != undefined) clearTimeout(root.timeout);
-            root.timeout = setTimeout(function(){
-                var base64ImageData = canvas.toDataURL("image/png");
-
-                root.socket.send(JSON.stringify({ event: "canvas-data", data: base64ImageData }));
-                console.log("sent data");
-            }, 1000) 
-        };
-
- 
-    }
-
-    render() {
-        return (
-            <div class="sketch" id="sketch">
-                <canvas className="board" id="board"></canvas>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div class="sketch" id="sketch">
+        <canvas className="board" id="board"></canvas>
+      </div>
+    );
+  }
 }
 
-export default Board
+export default Board;
