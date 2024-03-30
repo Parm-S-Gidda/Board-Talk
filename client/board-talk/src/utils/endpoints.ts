46cd6e4210584d2e08cad4328e428579ff270831
VinciVelasco <vincivelasco@gmail.com>
2024-03-30 15:00:28 -0700

    updated things .

--- client/board-talk/src/utils/endpoints.ts
+++ client/board-talk/src/utils/endpoints.ts
@@ -1,12 +1,12 @@
 export const SIGN_UP_END_POINT =
-  "https://user-service-nf23qxdm2q-uc.a.run.app/api/users/signup";
+  "https://user-service-2pmnanshaq-uc.a.run.app/api/users/signup";
 export const GET_QUESTIONS_END_POINT =
-  "https://question-service-nf23qxdm2q-uc.a.run.app/api/questions";
+  "https://question-service-2pmnanshaq-uc.a.run.app/api/questions";
 export const GET_USER_ENDPOINT =
-  "https://user-service-nf23qxdm2q-uc.a.run.app/api/users/";
+  "https://user-service-2pmnanshaq-uc.a.run.app/api/users/";
 export const GET_ANSWERS =
-  "https://question-service-nf23qxdm2q-uc.a.run.app/api/answers";
+  "https://question-service-2pmnanshaq-uc.a.run.app/api/answers";
 export const POST_ANSWER =
-  "https://question-service-nf23qxdm2q-uc.a.run.app/api/answers";
+  "https://question-service-2pmnanshaq-uc.a.run.app/api/answers";
 export const POST_GUESTION =
-  "https://question-service-nf23qxdm2q-uc.a.run.app/api/questions";
+  "https://question-service-2pmnanshaq-uc.a.run.app/api/questions";
