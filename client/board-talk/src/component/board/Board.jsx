10d5972dd02dc6d58c821520929494dbd9ed0b19
VinciVelasco <vincivelasco@gmail.com>
2024-03-30 15:33:41 -0700

    didn't add the correct file.

--- client/board-talk/src/component/board/Board.jsx
+++ client/board-talk/src/component/board/Board.jsx
@@ -15,7 +15,7 @@ class Board extends React.Component {
 
     this.socket = new WebSocket(
       "wss://" +
-        "whiteboard-service-nf23qxdm2q-uc.a.run.app" +
+        "whiteboard-service-2pmnanshaq-uw.a.run.app" +
         "/ws/whiteboard/" +
         1 +
         "/"
