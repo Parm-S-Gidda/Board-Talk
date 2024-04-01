6c4689ea2b7bd6a65590f3776387f10db855a723
Parm Gidda <pgidda0925@gmail.com>
2024-04-01 15:59:38 -0700

    deployment files added

--- client/board-talk/src/utils/endpoints.ts
+++ client/board-talk/src/utils/endpoints.ts
@@ -11,9 +11,9 @@
 // export const POST_GUESTION =
 //   "https://question-service-nf23qxdm2q-uc.a.run.app/api/questions";
 
-export const SIGN_UP_END_POINT = "http://localhost:5000/api/users/signup";
-export const GET_QUESTIONS_END_POINT = "http://localhost:7000/api/questions";
-export const GET_USER_ENDPOINT = "http://localhost:5000/api/users/";
-export const GET_ANSWERS = "http://localhost:7000/api/answers";
-export const POST_ANSWER = "http://localhost:7000/api/answers";
-export const POST_GUESTION = "http://localhost:7000/api/questions";
+export const SIGN_UP_END_POINT = "https://user-service-ljofwenvaq-uc.a.run.app/api/users/signup";
+export const GET_QUESTIONS_END_POINT = "https://question-service-ljofwenvaq-uc.a.run.app/api/questions";
+export const GET_USER_ENDPOINT = "https://user-service-ljofwenvaq-uc.a.run.app/api/users/";
+export const GET_ANSWERS = "https://question-service-ljofwenvaq-uc.a.run.app/api/answers";
+export const POST_ANSWER = "https://question-service-ljofwenvaq-uc.a.run.app/api/answers";
+export const POST_GUESTION = "https://question-service-ljofwenvaq-uc.a.run.app/api/questions";
