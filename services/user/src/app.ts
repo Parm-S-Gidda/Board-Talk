6d8ca07cde93349c85ab2f5a5e66eb5dd6d7114b
Ikenna Okpala <ikennajesse77@gmail.com>
2024-04-03 12:30:46 -0700

    Add compression

--- services/user/src/app.ts
+++ services/user/src/app.ts
@@ -2,6 +2,7 @@ import express, { Express, Request, Response } from "express";
 import userRouter from "./routes/user.route";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import compression from "compression";
 
 const app = express();
 
@@ -10,6 +11,8 @@ app.use(cors());
 app.use(express.json());
 
 app.use(cookieParser());
+
+app.use(compression());
 
 const port = process.env.port || 8080;
 
